5f52507e7e61a7df5f99e73227001c68a465df07
edscheff <evscheff@bu.edu>
2021-12-05 20:18:37 -0700

    update resource js

--- JS/resources.js
+++ JS/resources.js
@@ -1,9 +1,16 @@
 window.onload = function () {
     importResources();
-    console.log(jsonArray);
+    //console.log(jsonArray);
+    console.log(resource1);
 }
 
 var jsonArray = [];
+var resource1;
+var resource2;
+var resource3;
+var resource4;
+var resource5;
+var resource6;
 
 
 function importResources() {
@@ -16,9 +23,12 @@ function importResources() {
     })
         .then((data) => {
             console.log('Im working!');
-            for (var i in data) {
-                jsonArray.push(data[i]);
-            }
+            resource1 = data.resources[0];
+
+            // the below works
+            // for (var i in data) {
+            //     jsonArray.push(data[i]);
+            // }
         })
         .catch((error) => {
             console.log(error);
