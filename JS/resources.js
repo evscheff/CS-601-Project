f2c5a61e8ee367b86e0dd471dea5dbd6ba32853d
edscheff <evscheff@bu.edu>
2021-12-05 18:36:23 -0700

    first resources script

---
+++ JS/resources.js
@@ -0,0 +1,15 @@
+async function importResources() {
+    fetch('Data/resources.json').then((response) => {
+        if (response.ok) {
+            return response.json();
+        } else {
+            throw new Error('problem accessing file');
+        }
+    })
+        .then((data) => {
+            console.log('Im working!')
+        })
+        .catch((error) => {
+            console.log(error);
+        });
+}
